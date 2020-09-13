e98045a084bfac1d01b2007ce6c262f8cfb86782
Shubham <Shubham16.Gupta@ril.com>
2020-09-13 15:02:48 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/space-x'));
+app.use(express.static('./dist/space-x'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/space-x/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/space-x' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
